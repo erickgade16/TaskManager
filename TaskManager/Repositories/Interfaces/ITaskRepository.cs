ef9e4b210dff29b4d8a8f00f2e5df187b2be740e
erick <erick@DESKTOP-JBOT9NK>
2024-07-27 15:21:55 -0300

    Criando Interfaces do ro repositorio

---
+++ TaskManager/Repositories/Interfaces/ITaskRepository.cs
@@ -0,0 +1,10 @@
+﻿namespace TaskManager.Repositories.Interfaces
+{
+    public interface ITaskRepository
+    {
+        void AddTask(Task task);
+        void RemoveTask(int taskId);
+        void UpdateTask(Task task);
+        IEnumerable<Task> ListTasks();
+    }
+}
