85677a9de2d70d73458e9f8b7c35ce198bdf91f9
Erick Gadelha da Silva <erickgade16@gmail.com>
2025-05-21 05:04:00 -0300

    Arrumando problemas de nulos

--- TaskManager/Repositories/Interfaces/ITaskRepository.cs
+++ TaskManager/Repositories/Interfaces/ITaskRepository.cs
@@ -3,8 +3,9 @@
 public interface ITaskRepository
 {
     Task<IEnumerable<TaskViewModel>> ListTasks();
-    Task<TaskViewModel> GetTaskByIdAsync(int id);
-    Task<TaskViewModel> AddTask(TaskViewModel task);
-    Task<TaskViewModel> UpdateTask(TaskViewModel task);
-    Task<TaskViewModel> RemoveTask(int id);
+    Task<TaskViewModel?> GetTaskByIdAsync(int id);
+    Task<TaskViewModel?> AddTask(TaskViewModel task);
+    Task<TaskViewModel?> UpdateTask(TaskViewModel task);
+    Task<TaskViewModel?> RemoveTask(int id);
 }
+
